1e04a7d690d9cda47488a2a73b5c251db9775b20
Lucas Wolford <lucas.wolford@gmail.com>
2015-05-30 14:15:55 -0400

    Added a player and started working on movement.

---
+++ Assets/playerInput.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerInput : MonoBehaviour {
+	private Rigidbody2D body;
+	public float acceleration;
+	public float maxSpeed;
+	
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	
+	// All Physics go here
+	void FixedUpdate () {
+		Vector2 movement = new Vector2(Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+		body.AddForce (movement);
+	}
+}
