b2f3f459c2dfb1825e588de5d3f650144c0b8ff4
Robin Gersabeck <robin@splyt.com>
2015-05-30 17:50:40 -0400

    Players face they way they are inputting

--- Assets/playerInput.cs
+++ Assets/playerInput.cs
@@ -3,9 +3,9 @@ using System.Collections;
 
 public class playerInput : MonoBehaviour {
 	private Rigidbody2D body;
-	public float acceleration;
-	public float maxSpeed;
 	public int playerNum;
+
+	private float deadZone = 0.20f;
 	
 	// Use this for initialization
 	void Start () {
@@ -19,8 +19,17 @@ public class playerInput : MonoBehaviour {
 	
 	// All Physics go here
 	void FixedUpdate () {
-		Vector2 movement = new Vector2(Input.GetAxis ("Horizontal"+playerNum), Input.GetAxis ("Vertical"+playerNum));
+		float inputX = Input.GetAxis ("Horizontal" + playerNum);
+		float inputY = Input.GetAxis ("Vertical" + playerNum);
+		Vector2 movement = new Vector2(inputX, inputY);
 		movement.Scale (new Vector2 (15, 15));
 		body.AddForce (movement);
+
+		// Set the z rotation of that player to point at the way he is inputting
+		if (Mathf.Abs (inputX) > deadZone || Mathf.Abs (inputY) > deadZone) {
+			float angle = Mathf.Atan2 (inputY, inputX) * Mathf.Rad2Deg + 90f;
+
+			transform.localEulerAngles = new Vector3 (0, 0, angle);
+		}
 	}
 }
