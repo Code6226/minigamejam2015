810b24fe03a2319d7307e4d5f9084b0b98332c78
Lucas Wolford <lucas.wolford@gmail.com>
2015-05-30 21:08:01 -0400

    Added a food spawner.

---
+++ Assets/Scripts/FoodSpawn.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FoodSpawn : MonoBehaviour {
+
+	public int frequency;
+	public float posMaxX;
+	public float posMinX;
+	public float posMaxY;
+	public float posMinY;
+
+	private int lastSpawn;
+	private System.DateTime epochStart;
+
+	// Use this for initialization
+	void Start () { 
+		System.DateTime epochStart = new System.DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
+		
+		lastSpawn = (int)(System.DateTime.UtcNow - epochStart).TotalMilliseconds; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		int cur_time = (int)(System.DateTime.UtcNow - epochStart).TotalMilliseconds;
+
+		int spawn = cur_time - lastSpawn;
+		Debug.Log(spawn);
+
+		if (spawn >= frequency) {
+			var pos = new Vector2 (Random.Range (posMinX, posMaxX), Random.Range (posMinY, posMaxY));
+			GameObject prefab = (GameObject)Instantiate (Resources.Load ("FoodA"), pos, transform.rotation);
+
+			lastSpawn = (int)(System.DateTime.UtcNow - epochStart).TotalMilliseconds;
+		}
+
+
+	}
+}
