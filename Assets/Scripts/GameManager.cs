a0289d27c908a18b55a7a66104fe0337742980ee
Robin Gersabeck <robin@splyt.com>
2015-05-31 15:54:59 -0400

    keeping track of score; more kitties

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	private int scorePlayerOne = 0;
+	private int scorePlayerTwo = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void FixedUpdate () {
+	
+	}
+
+	public void addScore(int playerNum){
+		if (playerNum == 1) {
+			scorePlayerOne++;
+		} else {
+			scorePlayerTwo++;
+		}
+	}
+}
--- Assets/Scripts/Kitty.cs
+++ Assets/Scripts/Kitty.cs
@@ -8,15 +8,19 @@ public class Kitty : MonoBehaviour {
 	private FoodMeter foodMeter;
 	private int foodCount = 0;
 
+	private GameManager gameManager; 
+
 	// Use this for initialization
 	void Start () {
 		foodMeter = GetComponentInChildren<FoodMeter> ();
+		gameManager = GameObject.FindObjectOfType<GameManager> ();
 //		foodMeter.setCount (3);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		// animation time is forgotten after dis-re-enabling - so far picking up and dropping the kitty off elsewhere
+		foodMeter.setCount(foodCount);
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
@@ -25,6 +29,8 @@ public class Kitty : MonoBehaviour {
 				Destroy(other.gameObject);
 				foodCount++;
 				foodMeter.setCount(foodCount);
+
+				gameManager.addScore(playerNum);
 			}
 		}
 	}
