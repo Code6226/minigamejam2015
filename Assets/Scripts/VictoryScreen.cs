7f396107b57110375dd25ce72303d3299bc6d5a7
Robin Gersabeck <robin@splyt.com>
2015-06-01 00:03:53 -0400

    Victory sound plays on victory screens

---
+++ Assets/Scripts/VictoryScreen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VictoryScreen : MonoBehaviour {
+
+	public AudioClip audioWin;
+
+	// Use this for initialization
+	void Start () {
+		AudioSource.PlayClipAtPoint (audioWin, Vector3.zero);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
